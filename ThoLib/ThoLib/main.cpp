cb239bbd2240c116ab086e0719466577efd9cd0f
Aaron Hodel <aaronhodel@gmail.com>
2017-12-19 18:48:25 +0100

    Hello There

---
+++ ThoLib/ThoLib/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ThoLib
+//
+//  Created by Aaron Hodel on 19.12.17.
+//  Copyright © 2017 Aaron Hodel. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
